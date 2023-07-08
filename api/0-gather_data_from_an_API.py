c1bf737c5d2c5328815c3b8760d0970fb64fd028
lanaarafat <a.mutoni1@alustudent.com>
2023-07-08 22:36:27 +0200

    everything API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''requests'''
+"""requests"""
 import requests
 import sys
-'''requests'''
+"""requests"""
 if __name__ == "__main__":
     user_id = sys.argv[1]
     user_url = "https://jsonplaceholder.typicode.com/user/{}" \.format(user.id)

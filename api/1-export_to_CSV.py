f5de5aa99594458c483385fcfa574981a1cbf2e2
lanaarafat <a.mutoni1@alustudent.com>
2023-07-09 14:03:13 +0200

    everything api

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-'''csv'''
+"""json"""
+
+import csv
+import json
 import requests
 import sys
-import json
-import csv
-'''csv'''
-
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/user/{}" \.format(user.id)
-    todos_url = "https://jsonplaceholder.typicode.com/todos/{}" \.format(user_id)
-
-    user_info = requests.request('GET', user_url).json()
-    todo_info = requests.request('GET', todos_url).json()
-
-    employee_name = user_info["name"]
-    task_completed = list(filter(lambda obj: (obj["completed"] is True), todos_info))
-    number_of_done_tasks = len(task_completed)
-    total_number_of_tasks = len(todos_info)
-
-    with open('{}.csv'.format(user_id), 'w') as cvsfile:
-        csvwriter = csv.writer(csvfile, quoting=cvs.QUOTE_ALL)
-        [csvwriter.writerow([user_id, user_info["username"], task["completed"], task["title"]]) for task in todos_info]
-            
+    link = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    res = requests.get(link)
+    user = json.loads(res.text)
+    num = sys.argv[1]
+    link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
+    res = requests.get(link)
+    todos = json.loads(res.text)
+    csv_data = [["{}".format(i["userId"]),
+                user["username"],
+                "{}".format(i["completed"]),
+                 i["title"]] for i in todos]
+    with open("{}.csv".format(user["id"]), 'w', encoding='utf-8') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerows(csv_data)
